bc46acebab9913539925432bdbda8a439f92e6c5
dhobae <stmik.mridhosaputra@gmail.com>
2026-03-01 15:57:08 +0800

    adding sitemap xml

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://bebuhan-proseka.vercel.app',
+    })
+  ],
   resolve: {
     dedupe: ['react', 'react-dom', 'three', '@react-three/fiber', '@react-three/drei']
   }
